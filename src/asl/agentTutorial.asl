02ef1696b5251c6031f00205f4d109ca92f6aeed
Franzi <fb060>
2017-12-20 19:12:01 +0100

    Neues Tutorial vom 20.12.

--- src/asl/agentTutorial.asl
+++ src/asl/agentTutorial.asl
@@ -1,13 +1,18 @@
 // Goal
-!addition.
+!clean_all. 
+                  // initial goal
+
 
 // Belief Update Rules
-+sum(X) <-
-	.print("I perceive a result");	// print is an internal action provided by Jason, hence the dot at the beginning	
-	.print("Sum of 2+4 =", X).
+
+
 
 // Plans
-+!addition <-
-	.print("Started adding");
-	add(2,4);						// call environment action
-	.print("Finished adding").
+
++!clean_all <- clean; !move; !clean_all.
+
++!move:position(0)  <- right.
++!move:position(1)  <- down.
++!move:position(2)  <- left.
++!move:position(3)  <- up.
+
--- src/java/plotmas/tutorial/TutorialModel.java
+++ src/java/plotmas/tutorial/TutorialModel.java
@@ -2,18 +2,66 @@ package plotmas.tutorial;
 
 import java.util.List;
 
+import java.util.logging.Logger;
+
 import plotmas.PlotEnvironment;
 import plotmas.PlotLauncher.LauncherAgent;
 import plotmas.storyworld.Model;
 
 public class TutorialModel extends Model {
-
+	
+	static Logger logger = Logger.getLogger(TutorialModel.class.getName());
+	
 	public TutorialModel(List<LauncherAgent> agentList, PlotEnvironment<TutorialModel> env) {
 		super(agentList, env);
 	}
 	
-	public int add(int a, int b) {
-		return a + b;
-	}
-
+	
+	boolean [] state = new boolean[4];  // Felder mit Zustand dreckig oder sauber
+	public int position; 						//Position des Agenten
+		
+	
+	public void suck(){
+		if (state[position] == true){
+			state[position] = false;}  //true heiﬂt dreckig, false heiﬂt sauber! 
+			logger.info("position " + String.valueOf(position) + " is clean");
+		}
+	
+	
+	public void down() {
+		if (position == 0){
+				position = 3;
+		}
+		
+		if (position == 1) {
+				position =2;}
+		}
+	
+	public void left() {
+		if (position == 1){
+				position = 0;
+		}
+		
+		if (position == 2) {
+				position =3;}
+		}
+	
+	public void right() {
+		if (position == 0){
+				position = 1;
+		}
+		
+		if (position == 3) {
+				position =2;}
+		}
+	
+	public void up() {
+		if (position == 2){
+				position = 1;
+		}
+		
+		if (position == 3) {
+				position =0;}
+		}
 }
+
