d78ba82c70b46f22ccb52db9bcd4a7ac6ff1acc9
cartisan <leon@die-genossen.de>
2018-10-22 19:16:46 +0200

    Implemented heuristic function to extract personality conditions from affect annotations + test

--- src/java/plotmas/helper/TermParser.java
+++ src/java/plotmas/helper/TermParser.java
@@ -1,5 +1,7 @@
 package plotmas.helper;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -7,6 +9,7 @@ import jason.asSemantics.Emotion;
 import jason.asSyntax.ASSyntax;
 import jason.asSyntax.Literal;
 import jason.asSyntax.parser.ParseException;
+import jason.util.Pair;
 import plotmas.graph.Edge;
 
 /**
@@ -16,7 +19,9 @@ import plotmas.graph.Edge;
  */
 public class TermParser {
 	
-	private static final Pattern EmotionPattern = Pattern.compile("(?<emotion>\\w*)\\["+ Edge.Type.CAUSALITY.toString() +"\\((?<cause>.*)\\)\\]\\(.\\)");
+	private static final Pattern EMOTION_PATTERN = Pattern.compile("(?<emotion>\\w*)\\["+ Edge.Type.CAUSALITY.toString() +"\\((?<cause>.*)\\)\\]\\(.\\)");
+	public static final String PERSO_PATTERN = "personality\\((?<trait>.+?),(?<scope>.+?)\\)";
+			
 	/**
 	 * Creates an emotion object from a textual representation of an emotion
 	 * as it is created by {@link Emotion#toString()}. This means that the
@@ -26,7 +31,7 @@ public class TermParser {
 	 * @return Emotion object
 	 */
 	public static Emotion emotionFromString(String emo) {
-		Matcher matcher = EmotionPattern.matcher(emo);
+		Matcher matcher = EMOTION_PATTERN.matcher(emo);
 		if(matcher.find()) {
 			Emotion emotion = Emotion.getEmotion(matcher.group(Emotion.ANNOTATION_FUNCTOR).toLowerCase());
 			emotion.setCause(matcher.group(Edge.Type.CAUSALITY.toString()));
@@ -81,5 +86,31 @@ public class TermParser {
 			result += c;
 		}
 		return result;
+	}
+	
+	/**
+	 * Quick and dirty heuristic to extract personality preconditios from a plan annotation. Matches all instances of
+	 * personality(trait, scale) in annot and extracts them as pairs. Returns a list of all these pairs, standing in 
+	 * for all potential personalities that could enable this plan to be executed. <br><br>
+	 * 
+	 * <b>Attention:</b> This ignores all logical operators like <i>and</i>, <i>or</i>, <i>not</i> and does not take
+	 * into account the mood part of the annotations. While the latter is acceptable, as it is a dynamic property of the
+	 * simulation, the former is undesirable. A FIXME: would be to create a tree representation of the involved 
+	 * personality conditions and return all paths of the tree as potential personality conditions.
+	 * 
+	 * @param annot string representation of an affective plan precondition
+	 * @see plotmas.test.util.TermParserTest
+	 * @return
+	 */
+	public static List<Pair<String,String>> extractPersonalityAnnotation(String annot) {
+		Pattern pattern = Pattern.compile(PERSO_PATTERN, Pattern.MULTILINE);
+		Matcher matcher = pattern.matcher(annot);
+		
+		List<Pair<String,String>> conditions = new LinkedList<>();
+		while (matcher.find()) {
+			conditions.add(new Pair<String, String>(matcher.group("trait"), matcher.group("scope")));
+		}
+		
+		return conditions;
 	}
 }
---
+++ src/test/java/plotmas/test/util/TermParserTest.java
@@ -0,0 +1,29 @@
+package plotmas.test.util;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import jason.util.Pair;
+import junit.framework.TestCase;
+import plotmas.helper.TermParser;
+
+public class TermParserTest extends TestCase {
+	
+	public void testExtractPersonalityAnnotation() {
+		String annotation = "affect(personality(extraversion,positive)";
+		List<?> results = TermParser.extractPersonalityAnnotation(annotation);
+		List<Pair<String, String>> expected = new LinkedList<>();
+		expected.add(new Pair<String, String>("extraversion","positive"));
+		
+		assertEquals(expected, results);
+		
+		annotation = "affect(or(and(personality(extraversion,positive),not (mood(dominance,low))),personality(openness,low)),source(self)";
+		results = TermParser.extractPersonalityAnnotation(annotation);
+		expected = new LinkedList<>();
+		expected.add(new Pair<String, String>("extraversion","positive"));
+		expected.add(new Pair<String, String>("openness","low"));
+		
+		assertEquals(expected, results);
+	}
+
+}
