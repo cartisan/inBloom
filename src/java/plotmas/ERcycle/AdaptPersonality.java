39f299d81583f2c3f72ecdddf8b74aba04481d16
cartisan <leon@die-genossen.de>
2018-10-24 19:20:21 +0200

    Working on allowing AdaptPersonality to take into account each step of a plan, not just the first

--- src/java/plotmas/ERcycle/AdaptPersonality.java
+++ src/java/plotmas/ERcycle/AdaptPersonality.java
@@ -33,15 +33,16 @@ public class AdaptPersonality implements ProblemFixCommand {
     	VALUE_MAP.put("high",   1.0);
     }
     
-    /**
-	 * 
-     * @param unresolvedHappening
-     * @param charName
-     * @return
-     */
+    private static Map<String, Personality> fixMap = new HashMap<>();
+
     public static AdaptPersonality getNextFixFor(String unresolvedHappening, String charName) {
     	// TODO: Implement backtracking, so that all solutions for a happening are successively explored
-    	return new AdaptPersonality(unresolvedHappening, charName);
+    	if (fixMap.containsKey(unresolvedHappening + "#" + charName)) {
+    		return null;
+    	}
+    	AdaptPersonality fix = new AdaptPersonality(unresolvedHappening, charName);
+    	fixMap.put(unresolvedHappening + "#" + charName, fix.persDiff);
+    	return fix;
     }
 	
 	private PlanLibrary planLib;
@@ -56,44 +57,55 @@ public class AdaptPersonality implements ProblemFixCommand {
 		
 		// Find which plans could get triggered by this happening and identify their pre-conditions
 		List<Plan> candidatePlans = planLib.getCandidatePlans(Trigger.parseTrigger(unresolvedHappening));
-		List<Literal> affectConditions = new LinkedList<>(); 
+		List<LinkedList<Literal>> affectConditions = new LinkedList<>(); 
 		for (Plan p : candidatePlans) {
+			LinkedList<Literal> conditionList = new LinkedList<>();
+			affectConditions.add(conditionList);
+
+			// TODO: iterate over all steps in body
 			String firstStep = p.getBody().getBodyTerm().toString();
-			// for each candidate plan, check affective preconditions (and context?) of its first step
-			List<Plan> firstStepOptions = planLib.getCandidatePlans(Trigger.parseTrigger("+!" + firstStep));
-			
-			// FIXME: this is not appropriate: reasoner always selects first fitting plan, no matter if its ground/annotations are present
-			//        need to take preconditions into account?
-			firstStepOptions = firstStepOptions.stream().filter(x -> x.getTrigger().getLiteral().isGround())  // filter out generic plans like +X! <-- they are not ground
-					.filter(x -> x.getLabel().getAnnot(Affect.ANNOTATION_FUNCTOR) != null)  // filter out plans without affective preconditions, no need to change personalities there
-					.collect(Collectors.toList());
-			
-			// this would give us the precondition for a coping plan, if we were to test that preconditions are met:
-			//List<LogicalFormula> contexts = firstStepOptions.stream().map(x -> x.getContext()).collect(Collectors.toList());
-			
-			// choose one option to pursue; if none available, note that
-			if (!firstStepOptions.isEmpty()) {
-				Plan selectedPlan = (Plan) SELECTION_STRATEGY.apply(firstStepOptions);
-				affectConditions.add(selectedPlan.getLabel().getAnnot(Affect.ANNOTATION_FUNCTOR));
-			} else {
-				affectConditions.add(null);
-			}
+			this.findPlanPreconditions(conditionList, firstStep);
 		}
 		
 		// for each candidatePlan, affectConditions now contains either affect-precondition of first step, or null if candidatePlan is no viable
 		assert(candidatePlans.size() == affectConditions.size());
 		
-		List<Literal> viablePlansPreConditions = affectConditions.stream().filter(x -> x != null).collect(Collectors.toList());
-		Literal selectedCondition = (Literal) SELECTION_STRATEGY.apply(viablePlansPreConditions);
+		List<List<Literal>> viablePlansPreConditions = affectConditions.stream().filter(x -> x.size() > 0).collect(Collectors.toList());
+		List<Literal> selectedCondition = (List<Literal>) SELECTION_STRATEGY.apply(viablePlansPreConditions);
 		
-		// derive personality from condition
-		List<Pair<String, String>> viablePersonalities = TermParser.extractPersonalityAnnotation(selectedCondition.toString());
-		Pair<String, String> selectedPersonality = (Pair<String, String>) SELECTION_STRATEGY.apply(viablePersonalities); 
-		
-		// compute new personality: traits with value 0 will not change oldPers, all other traits will be changed to new Pers
+		// derive personality conditions from collected annotations in selectedCondition
 		this.persDiff = Personality.createDefaultPersonality();
-		this.persDiff.setTrait(selectedPersonality.getFirst(),
-							  VALUE_MAP.get(selectedPersonality.getSecond()));
+		for (Literal annot : selectedCondition) {
+			List<Pair<String, String>> viablePersonalities = TermParser.extractPersonalityAnnotation(annot.toString());
+			Pair<String, String> selectedPersonality = (Pair<String, String>) SELECTION_STRATEGY.apply(viablePersonalities); 
+		
+			// compute new personality: traits with value 0 will not change oldPers, all other traits will be changed to new Pers
+			this.persDiff.setTrait(selectedPersonality.getFirst(),
+								   VALUE_MAP.get(selectedPersonality.getSecond()));
+		}
+	}
+
+	protected void findPlanPreconditions(List<Literal> affectConditions, String planStep) {
+		// for each candidate plan, check affective preconditions (and context?) of its first step
+		List<Plan> firstStepOptions = planLib.getCandidatePlans(Trigger.parseTrigger("+!" + planStep));
+		
+		// FIXME: this is not appropriate: reasoner always selects first fitting plan, no matter if its ground/annotations are present
+		//        need to take preconditions into account?
+		firstStepOptions = firstStepOptions.stream().filter(x -> x.getTrigger().getLiteral().isGround())  // filter out generic plans like +X! <-- they are not ground
+				.filter(x -> x.getLabel().getAnnot(Affect.ANNOTATION_FUNCTOR) != null)  // filter out plans without affective preconditions, no need to change personalities there
+				.filter(x -> x.getLabel().getAnnot(Affect.ANNOTATION_FUNCTOR).toString().contains(Personality.ANNOTATION_FUNCTOR))  // filter out plans without affective preconditions, no need to change personalities there
+				.collect(Collectors.toList());
+		
+		// this would give us the precondition for a coping plan, if we were to test that preconditions are met:
+		//List<LogicalFormula> contexts = firstStepOptions.stream().map(x -> x.getContext()).collect(Collectors.toList());
+		
+		// choose one option to pursue; if none available, note that
+		if (!firstStepOptions.isEmpty()) {
+			Plan selectedPlan = (Plan) SELECTION_STRATEGY.apply(firstStepOptions);
+			affectConditions.add(selectedPlan.getLabel().getAnnot(Affect.ANNOTATION_FUNCTOR));
+		} else {
+//			affectConditions.add(null);
+		}
 	}
 	
 	@Override
