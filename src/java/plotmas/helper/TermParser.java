9faa3cd0ae0cac60770656af2fb2588d79b03981
cartisan <leon@die-genossen.de>
2018-11-15 20:17:41 +0100

    First version of personality term parsing through CSP resolution

--- src/java/plotmas/helper/TermParser.java
+++ src/java/plotmas/helper/TermParser.java
@@ -1,10 +1,19 @@
 package plotmas.helper;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.chocosolver.solver.Model;
+import org.chocosolver.solver.Solution;
+import org.chocosolver.solver.Solver;
+import org.chocosolver.solver.constraints.Constraint;
+import org.chocosolver.solver.variables.IntVar;
+
+import jason.asSemantics.Affect;
 import jason.asSemantics.Emotion;
 import jason.asSyntax.ASSyntax;
 import jason.asSyntax.Literal;
@@ -21,6 +30,7 @@ public class TermParser {
 	
 	private static final Pattern EMOTION_PATTERN = Pattern.compile("(?<emotion>\\w*)\\["+ Edge.Type.CAUSALITY.toString() +"\\((?<cause>.*)\\)\\]\\(.\\)");
 	public static final String PERSO_PATTERN = "personality\\((?<trait>.+?),(?<scope>.+?)\\)";
+	public static final String AFFECT_PATTERN = "affect\\((?<trait>.+?),(?<scope>.+?)\\)";
 			
 	/**
 	 * Creates an emotion object from a textual representation of an emotion
@@ -112,5 +122,93 @@ public class TermParser {
 		}
 		
 		return conditions;
+	}
+	
+	// Returns a list of OCEAN tuples
+	public static List<List<Pair<String,Double>>> solutionsForPersonalityAnnotation(String annot) {
+        Literal annotLiteral = Literal.parseLiteral(annot);
+        
+        String annFunctor = annotLiteral.getFunctor();
+        if(!annFunctor.equals(Affect.ANNOTATION_FUNCTOR)) {
+        	throw new RuntimeException("Annotation should have functor 'affect', not: " + annFunctor);
+        }
+        
+        //affect(pers(C,h))    affect(and(p(C,h),p(E,l)))
+		Model model = new Model("pers-model");
+		Map<String,IntVar> intVarCache = new HashMap<>();
+        Constraint personalityConstraint = constrainFromAffectLiteral((Literal) annotLiteral.getTerm(0), model, intVarCache, "");
+        personalityConstraint.post();
+        
+        Solver solver = model.getSolver();
+        solver.showSolutions();
+        List<Solution> solutions = solver.findAllSolutions();
+        
+        List<List<Pair<String,Double>>> results = new LinkedList<>();
+        for (Solution s:solutions) {
+        	List<Pair<String, Double>> oceanConstraints = new LinkedList<>();
+        	
+        	for(String trait: intVarCache.keySet()) {
+        		IntVar var = intVarCache.get(trait);
+        		oceanConstraints.add(new Pair<String, Double>(trait, (s.getIntVal(var) / 10.0)));
+        		
+        	}
+        	
+        	results.add(oceanConstraints);
+        }
+        
+        return results;
+	}
+        
+		
+    private static Constraint constrainFromAffectLiteral(Literal lit, Model model, Map<String,IntVar> varCache, String parentFunc) {
+		String func = lit.getFunctor();
+		
+		switch(func) {
+    		case "mood": {
+    			switch(parentFunc) {
+    			case "or":
+    			case "not":	return model.falseConstraint();
+    			case "and":
+    			default: 	return model.trueConstraint();
+    			}
+    		}
+    		case "personality": {
+    			String trait = lit.getTerm(0).toString();
+    			String value = lit.getTerm(1).toString();
+    			
+    			varCache.putIfAbsent(trait, model.intVar(trait, new int[]{-10, -3, 3, 10}));
+    			IntVar var = varCache.get(trait);
+    			
+    			switch(value) {
+    			case "positive": return model.arithm(var, ">", 0);
+    			case "negative": return model.arithm(var, "<", 0);
+    			case "low": 	 return model.arithm(var, "<=", -7);
+    			case "medium": 	 return model.absolute(model.intVar(3), var) ;
+    			case "high": 	 return model.arithm(var, ">=", 7);
+    			default:		 throw new RuntimeException("Illegal trait-value: " + value);
+    			}
+    			
+    		}
+    		case "and":	{
+    			Constraint c0 = constrainFromAffectLiteral((Literal) lit.getTerm(0), model, varCache, func);
+    			Constraint c1 = constrainFromAffectLiteral((Literal) lit.getTerm(1), model, varCache, func);
+    			return model.and(c0, c1);
+    		}
+    		case "or":	{
+    			Constraint c0 = constrainFromAffectLiteral((Literal) lit.getTerm(0), model, varCache, func);
+    			Constraint c1 = constrainFromAffectLiteral((Literal) lit.getTerm(1), model, varCache, func);
+    			return model.or(c0, c1);
+    		}
+    		// for some reason Jason adds a space to our nots
+    		case "not ":	{
+    			if(((Literal) lit.getTerm(0)).getFunctor() == "mood") {
+    				// all mood's resolve to 1, so not(mood(X)) would always be 0 --> fix w/ lookahead 
+    				return model.trueConstraint(); 
+    			}
+    			Constraint c0 = constrainFromAffectLiteral((Literal) lit.getTerm(0), model, varCache, func);
+    			return model.not(c0);
+    		}
+    		default:	throw new RuntimeException("Illegal functor-value: " + func);
+		}
 	}
 }
--- src/test/java/plotmas/test/helper/TermParserTest.java
+++ src/test/java/plotmas/test/helper/TermParserTest.java
@@ -10,14 +10,14 @@ import plotmas.helper.TermParser;
 public class TermParserTest extends TestCase {
 	
 	public void testExtractPersonalityAnnotation() {
-		String annotation = "affect(personality(extraversion,positive)";
+		String annotation = "affect(personality(extraversion,positive))";
 		List<?> results = TermParser.extractPersonalityAnnotation(annotation);
 		List<Pair<String, String>> expected = new LinkedList<>();
 		expected.add(new Pair<String, String>("extraversion","positive"));
 		
 		assertEquals(expected, results);
 		
-		annotation = "affect(or(and(personality(extraversion,positive),not (mood(dominance,low))),personality(openness,low)),source(self)";
+		annotation = "affect(or(and(personality(extraversion,positive),not (mood(dominance,low))),personality(openness,low))),source(self)";
 		results = TermParser.extractPersonalityAnnotation(annotation);
 		expected = new LinkedList<>();
 		expected.add(new Pair<String, String>("extraversion","positive"));
@@ -26,4 +26,19 @@ public class TermParserTest extends TestCase {
 		assertEquals(expected, results);
 	}
 
+	public void testSolutionsForPersonalityAnnotation() {
+		String annotation = "affect(personality(extraversion,positive))";
+		List<List<Pair<String,Double>>> results = TermParser.solutionsForPersonalityAnnotation(annotation);
+		
+		assertEquals(2, results.size());
+		assertEquals("extraversion", results.get(0).get(0).getFirst());
+		assertEquals(0.3, results.get(0).get(0).getSecond());
+		assertEquals("extraversion", results.get(1).get(0).getFirst());
+		assertEquals(1.0, results.get(1).get(0).getSecond());
+		
+		// TODO: Solution not complete, also not simplified
+		annotation = "affect(or(and(personality(extraversion,positive),not (mood(dominance,low))),personality(openness,low))),source(self)";
+		results = TermParser.solutionsForPersonalityAnnotation(annotation);
+
+	}
 }
