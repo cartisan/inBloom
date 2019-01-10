25ef779f6a3dcedcf37440f3f67fcb92c4c8698e
cartisan <leon@die-genossen.de>
2019-01-10 19:42:22 +0100

    Fixed TermParserTest errors, perhaps needs more tests though

--- src/test/java/plotmas/test/helper/TermParserTest.java
+++ src/test/java/plotmas/test/helper/TermParserTest.java
@@ -2,11 +2,14 @@ package plotmas.test.helper;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 import com.google.common.collect.ImmutableSet;
 
+import jason.asSyntax.Literal;
 import jason.util.Pair;
 import junit.framework.TestCase;
+import plotmas.ERcycle.AdaptPersonality.OCEANConstraints;
 import plotmas.helper.TermParser;
 
 public class TermParserTest extends TestCase {
@@ -29,26 +32,24 @@ public class TermParserTest extends TestCase {
 	}
 
 	public void testSolutionsForPersonalityAnnotation() {
-		String annotation = "affect(personality(extraversion,positive))";
-		List<List<Pair<String,Double>>> results = TermParser.solutionsForPersonalityAnnotation(annotation);
+		Literal annotation = Literal.parseLiteral("affect(personality(extraversion,positive))");
+		Set<OCEANConstraints> results = TermParser.solutionsForPersonalityAnnotation(annotation);
 		
 		assertEquals(2, results.size());
-		assertEquals("extraversion", results.get(0).get(0).getFirst());
-		assertEquals(0.3, results.get(0).get(0).getSecond());
-		assertEquals("extraversion", results.get(1).get(0).getFirst());
-		assertEquals(1.0, results.get(1).get(0).getSecond());
+		assertEquals((Integer) 10, new LinkedList<OCEANConstraints>(results).get(1).getTrait("extraversion"));
+		assertEquals((Integer) 3, new LinkedList<OCEANConstraints>(results).get(0).getTrait("extraversion"));
 		
-		annotation = "affect(or(personality(extraversion,high),personality(openness,low))),source(self)";
+		annotation = Literal.parseLiteral("affect(or(personality(extraversion,high),personality(openness,low))),source(self)");
 		results = TermParser.solutionsForPersonalityAnnotation(annotation);
 		assertEquals(7, results.size());
 				
-		annotation = "affect(or(personality(extraversion,positive),personality(openness,low))),source(self)";
+		annotation = Literal.parseLiteral("affect(or(personality(extraversion,positive),personality(openness,low))),source(self)");
 		results = TermParser.solutionsForPersonalityAnnotation(annotation);
 		assertEquals(10, results.size());
 		
-		// TODO: also not simplified
-		annotation = "affect(or(and(personality(extraversion,positive),not (mood(dominance,low))),personality(openness,low))),source(self)";
-		List<List<Pair<String,Double>>> results2 = TermParser.solutionsForPersonalityAnnotation(annotation);
+		// Mood doesn't play a role in personality annotations
+		annotation = Literal.parseLiteral("affect(or(and(personality(extraversion,positive),not (mood(dominance,low))),personality(openness,low))),source(self)");
+		Set<OCEANConstraints> results2 = TermParser.solutionsForPersonalityAnnotation(annotation);
 		assertEquals(ImmutableSet.copyOf(results), ImmutableSet.copyOf(results2));
 	}
 }
