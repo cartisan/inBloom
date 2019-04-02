36adab35c4ed3741be070fe18ef8945c45fb4d33
cartisan <leon@die-genossen.de>
2019-04-02 16:11:49 +0200

    Reset Tutorial to pristine state, also taking care of new state handling

--- src/asl/agentTutorial.asl
+++ src/asl/agentTutorial.asl
@@ -1,44 +1,13 @@
 // Goal
-!cleaning.
+!addition.
 
 // Belief Update Rules
-+pos(X) <-
-	.print("My position is ", X+1).
-
-+dirty: dirty <-
-	.print("I perceive my position is dirty.").	
-	
-+clean: clean <-
-	.print("I perceive my position is clean.").	
-	
-
++sum(X) <-
+	.print("I perceive a result");	// print is an internal action provided by Jason, hence the dot at the beginning	
+	.print("Sum of 2+4 =", X).
 
 // Plans
-
-// plans for dirty location
-+!cleaning: dirty <- 
-	.print("Start cleaning at position ",X+1);
-	suck;
-	!move;
-	!cleaning.		
-	
-+!cleaning: clean <- 
-	.print("Position ",X+1, " is clean");
-	!move;
-	!cleaning.				
-	
-+!move: pos(0) <-
-	.print("Moving to next position.");
-	right.
-
-+!move: pos(1) <-						
-	.print("Moving to next position.");
-	down.
-
-+!move: pos(2) <-
-	.print("Moving to next position.");
-	left.
-	
-+!move: pos(3) <-
-	.print("Moving to next position.");
-	up.
++!addition <-
+	.print("Started adding");
+	add(2,4);						// call environment action
+	.print("Finished adding").
--- src/java/plotmas/tutorial/TutorialEnviroment.java
+++ src/java/plotmas/tutorial/TutorialEnviroment.java
@@ -7,66 +7,22 @@ import plotmas.PlotEnvironment;
 public class TutorialEnviroment extends PlotEnvironment<TutorialModel> {
 
     @Override
-	protected void updateStatePercepts(String agentName) {
-    	super.updateStatePercepts(agentName);
-    	
-    	// update publicly known wheat state
-		removePerceptsByUnif(Literal.parseLiteral("dirty"));
-		removePerceptsByUnif(Literal.parseLiteral("clean"));
-		removePerceptsByUnif(Literal.parseLiteral("pos(X)"));
-		addPercept(Literal.parseLiteral("pos("+ getModel().position + ")"));
-		if (getModel().dirty[getModel().position] == true) {
-			addPercept(Literal.parseLiteral("dirty"));
-		}
-		else addPercept(Literal.parseLiteral("clean"));
-    }
-	
-    @Override
-	public boolean doExecuteAction(String agentName, Structure action) {
+    public boolean doExecuteAction(String agentName, Structure action) {
     	boolean result = false;
-    
-		// check which action is executed by agent       
-		if (action.getFunctor().equals("suck")){
-			result = getModel().suck();
-			// create a perception of the result, which the agent receives
-//			addPercept(Literal.parseLiteral(String.format("clean")));
-			// indicate that action was successful
-			result = true;		
-      }
-		
-		if (action.getFunctor().equals("left")) {	
-			result = getModel().left();
-			// create a perception of the result, which the agent receives
-//			addPercept( Literal.parseLiteral(String.format("pos("+ getModel().position + ")")));
-			// indicate that action was successful
-			result = true;		
-      }
-		
-		if (action.getFunctor().equals("right")) {
-			result = getModel().right();
-			// create a perception of the result, which the agent receives
-//			addPercept( Literal.parseLiteral(String.format("pos("+ getModel().position + ")")));
-			// indicate that action was successful
-			result = true;		
-      }
-		
-		if (action.getFunctor().equals("up")) {
-			result = getModel().up();
-			// create a perception of the result, which the agent receives
-//			addPercept( Literal.parseLiteral(String.format("pos("+ getModel().position + ")")));
-			// indicate that action was successful
-			result = true;	
-      }
-		
-		if (action.getFunctor().equals("down")) {
-			result = getModel().down();
-			// create a perception of the result, which the agent receives
-//			addPercept( Literal.parseLiteral(String.format("pos("+ getModel().position + ")")));
-			// indicate that action was successful
-			result = true;	
+    	
+		// check which action is executed by agent
+		if (action.getFunctor().equals("add")) {
+			// parse the term values passed by agent, pass them on to model
+			int sum = this.model.add(Integer.valueOf( action.getTerm(0).toString() ),
+									 Integer.valueOf( action.getTerm(1).toString() ));
 			
+			// create a perception of the result, which the agent receives
+			addPercept(agentName, Literal.parseLiteral(String.format("sum(%d)", sum)));
+			
+			// indicate that action was successful
+			result = true;
       }
+
     	return result;
     }
-
 }
