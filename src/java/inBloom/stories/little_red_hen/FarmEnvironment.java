12c2a35de6273d83d1cf164fb9b664981acf568b
cartisan <leon@die-genossen.de>
2019-07-19 18:17:51 +0200

    Coding style

--- src/java/inBloom/stories/little_red_hen/FarmEnvironment.java
+++ src/java/inBloom/stories/little_red_hen/FarmEnvironment.java
@@ -4,14 +4,15 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.logging.Logger;
 
+import jason.asSyntax.ListTermImpl;
+import jason.asSyntax.Structure;
+import jason.asSyntax.Term;
+
 import inBloom.ActionReport;
 import inBloom.LauncherAgent;
 import inBloom.PlotEnvironment;
 import inBloom.storyworld.Character;
 import inBloom.storyworld.Location;
-import jason.asSyntax.ListTermImpl;
-import jason.asSyntax.Structure;
-import jason.asSyntax.Term;
 
 /**
  * Custom controller for the "Tale of the Little Red Hen", managed by {@link RedHenLauncher}.
@@ -19,7 +20,7 @@ import jason.asSyntax.Term;
  */
 public class FarmEnvironment extends PlotEnvironment<FarmModel> {
     static Logger logger = Logger.getLogger(FarmEnvironment.class.getName());
-    
+
 	@Override
     public void initialize(List<LauncherAgent> agents) {
 		super.initialize(agents);
@@ -27,93 +28,94 @@ public class FarmEnvironment extends PlotEnvironment<FarmModel> {
 		// Add special initialization of agent percepts for custom environment here.
 		// Left blank intentionally.
 	}
-    
+
 	@Override
     protected ActionReport doExecuteAction(String agentName, Structure action) {
 		ActionReport result = null;
-		Character agent = getModel().getCharacter(agentName);
-    	
+		Character agent = this.getModel().getCharacter(agentName);
+
     	if (action.getFunctor().equals("farm_work")) {
-    		result = getModel().farmWork(agent);
+    		result = this.getModel().farmWork(agent);
     	}
-    	
+
     	else if (action.getFunctor().equals("plant")) {
-			result = getModel().plantWheat(agent);
+			result = this.getModel().plantWheat(agent);
     	}
-    	
+
     	else if (action.toString().equals("tend(wheat)")) {
-    		result = getModel().tendWheat(agent);
+    		result = this.getModel().tendWheat(agent);
     	}
-    	
+
     	else if (action.toString().equals("harvest(wheat)")) {
-    		result = getModel().harvestWheat(agent);
+    		result = this.getModel().harvestWheat(agent);
     	}
-    	
+
     	else if (action.toString().equals("grind(wheat)")) {
-    		result = getModel().grindWheat(agent);
+    		result = this.getModel().grindWheat(agent);
     	}
-    	
+
     	else if (action.getFunctor().equals("bake")) {
-    		result = getModel().bakeBread(agent);
+    		result = this.getModel().bakeBread(agent);
     	}
-    	
+
     	else if (action.getFunctor().equals("eat")) {
     		String item = action.getTerm(0).toString();
     		result = agent.eat(item);
     	}
 
+    	// TODO: invent a semantics for help (ideally one fitting the Honored Request FU)
     	else if (action.getFunctor().equals("help")) {
     		result = new ActionReport(true);
     	}
-    	
+
     	else if (action.getFunctor().equals("share")) {
     		String item = action.getTerm(0).toString();
     		Term receiverTerm = action.getTerm(1);
-    		
+
     		if (receiverTerm.isList()) {
     			List<Character> receivers = new LinkedList<>();
     			for (Term rec: (ListTermImpl) receiverTerm) {
-        			receivers.add(getModel().getCharacter(rec.toString()));
+        			receivers.add(this.getModel().getCharacter(rec.toString()));
     			}
     			result = agent.share(item, receivers);
     		} else {
-    			Character patient = getModel().getCharacter(receiverTerm.toString());
+    			Character patient = this.getModel().getCharacter(receiverTerm.toString());
     			result = agent.share(item, patient);
-    			
+
     		}
     	}
-    	
+
     	else if (action.getFunctor().equals("goTo")) {
-    		Location target = getModel().getLocation(action.getTerm(0).toString());
+    		Location target = this.getModel().getLocation(action.getTerm(0).toString());
     		result = agent.goTo(target);
     	}
-    	
+
     	else if (action.getFunctor().equals("sing")) {
 			result = agent.sing();
     	}
-    	
+
     	else if (action.getFunctor().equals("collect")) {
     		String item = action.getTerm(0).toString();
-    		result = agent.collect(item ); 
+    		result = agent.collect(item );
     	}
-    	
+
     	else if (action.getFunctor().equals("handOver")) {
-    		Character receiver = getModel().getCharacter(action.getTerm(0).toString());
+    		Character receiver = this.getModel().getCharacter(action.getTerm(0).toString());
     		String item = action.getTerm(1).toString();
 			result = agent.handOver(receiver, item);
     	}
-    	
+
     	else if (action.getFunctor().equals("refuseHandOver")) {
     		// TODO: rather in speech act?
-    		Character target = getModel().getCharacter(action.getTerm(0).toString());
+    		Character target = this.getModel().getCharacter(action.getTerm(0).toString());
     		String item = action.getTerm(1).toString();
     		result = agent.refuseHandOver(target, item);
     	}
-    	
+
     	else if (action.getFunctor().equals("relax")) {
 			result = agent.relax();
     	}
-    	
+
     	return result;
     }
 }
--- src/java/inBloom/stories/little_red_hen/FarmModel.java
+++ src/java/inBloom/stories/little_red_hen/FarmModel.java
@@ -4,6 +4,9 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import jason.asSyntax.ASSyntax;
+import jason.asSyntax.Literal;
+
 import inBloom.ActionReport;
 import inBloom.LauncherAgent;
 import inBloom.PlotModel;
@@ -13,8 +16,6 @@ import inBloom.storyworld.HappeningDirector;
 import inBloom.storyworld.Item;
 import inBloom.storyworld.Location;
 import inBloom.storyworld.ModelState;
-import jason.asSyntax.ASSyntax;
-import jason.asSyntax.Literal;
 
 
 /**
@@ -29,73 +30,73 @@ public class FarmModel extends PlotModel<FarmEnvironment>{
 		super(agents, hapDir);
 
 		// add locations
-		this.addLocation(tree);
-		this.addLocation(farm);
+		this.addLocation(this.tree);
+		this.addLocation(this.farm);
 	}
 
 	public ActionReport farmWork(Character agent) {
-		if(agent.location == farm) {
-			farm.farmingProgress += 1;
+		if(agent.location == this.farm) {
+			this.farm.farmingProgress += 1;
 			logger.info("Some farming activity was performed");
 			return new ActionReport(true);
 		}
 		return new ActionReport(false);
 	}
-	
+
 	public ActionReport plantWheat(Character agent) {
 		ActionReport res = new ActionReport();
-		
-		if ((agent.location == farm) & agent.has(Wheat.itemName) ){
+
+		if (agent.location == this.farm & agent.has(Wheat.itemName) ){
 			Wheat wheatItem = (Wheat) agent.get(Wheat.itemName);
 			if (wheatItem.state == Wheat.STATES.SEED) {
 				agent.removeFromInventory(wheatItem);
-				farm.produce = wheatItem;
-				farm.updateProduceState(Wheat.STATES.GROWING);
-				
+				this.farm.produce = wheatItem;
+				this.farm.updateProduceState(Wheat.STATES.GROWING);
+
 				logger.info("Wheat planted");
 				res.addPerception(agent.name,PerceptAnnotation.fromEmotion("pride"));
-				
+
 				res.success = true;
 			}
 		}
 
 		return res;
 	}
-	
+
 	public ActionReport tendWheat(Character agent) {
 		ActionReport res = new ActionReport();
-		
-		if ((agent.location == farm) & (farm.produce.state == Wheat.STATES.GROWING)){
-			farm.updateProduceState(Wheat.STATES.RIPE);
+
+		if (agent.location == this.farm & this.farm.produce.state == Wheat.STATES.GROWING){
+			this.farm.updateProduceState(Wheat.STATES.RIPE);
 			logger.info("Wheat has grown and is ripe now");
 			res.addPerception(agent.name, PerceptAnnotation.fromEmotion("pride"));
 			res.success = true;
 		}
-		
+
 		return res;
 	}
-	
+
 	public ActionReport harvestWheat(Character agent) {
 		ActionReport res = new ActionReport();
-		
-		if ((agent.location == farm) & (farm.produce.state == Wheat.STATES.RIPE)){
-			Wheat w =  farm.produce;
+
+		if (agent.location == this.farm & this.farm.produce.state == Wheat.STATES.RIPE){
+			Wheat w =  this.farm.produce;
 			w.state = Wheat.STATES.HARVESTED;
 			agent.addToInventory(w);
-			farm.updateProduceState(null);
-			
+			this.farm.updateProduceState(null);
+
 			logger.info("Wheat was harvested");
 			res.addPerception(agent.name, PerceptAnnotation.fromEmotion("pride"));
 			res.success = true;
 		}
-		
+
 		return res;
 	}
-	
+
 	public ActionReport grindWheat(Character agent) {
 		ActionReport res = new ActionReport();
-		
-		if ((agent.location == farm) & agent.has(Wheat.itemName) & (((Wheat) agent.get(Wheat.itemName)).state == Wheat.STATES.HARVESTED)){
+
+		if (agent.location == this.farm & agent.has(Wheat.itemName) & ((Wheat) agent.get(Wheat.itemName)).state == Wheat.STATES.HARVESTED){
 			Wheat w =  (Wheat) agent.removeFromInventory(Wheat.itemName);
 			w.state = Wheat.STATES.FLOUR;
 			agent.addToInventory(w);
@@ -108,38 +109,38 @@ public class FarmModel extends PlotModel<FarmEnvironment>{
 
 	public ActionReport bakeBread(Character agent) {
 		ActionReport res = new ActionReport();
-		
-		if ((agent.location == farm) & agent.has(Wheat.itemName) & (((Wheat) agent.get(Wheat.itemName)).state == Wheat.STATES.FLOUR)){
+
+		if (agent.location == this.farm & agent.has(Wheat.itemName) & ((Wheat) agent.get(Wheat.itemName)).state == Wheat.STATES.FLOUR){
 			Wheat wheatItem = (Wheat) agent.get(Wheat.itemName);
 			agent.removeFromInventory(wheatItem);
 			agent.addToInventory(new Bread());
-			
+
 			logger.info(agent.name + ": baked some bread.");
 			res.addPerception(agent.name, new PerceptAnnotation("pride", "joy"));
 			res.success =  true;
 		}
-		
+
 		return res;
 	}
-	
+
 	/****** helper classes *******/
 	public static class Farm extends Location {
-		
-		@ModelState 
+
+		@ModelState
 		public Wheat produce;
-		
-		@ModelState 
+
+		@ModelState
 		public int farmingProgress;
-		
+
 		public Farm() {
 			super("farm");
 			this.farmingProgress = 0;
 			this.produce = null;
 		}
-		
+
 		public void updateProduceState(Wheat.STATES state) {
 			this.model.environment.removePerceptsByUnif(Literal.parseLiteral("at(wheat[X]," + this.literal() + ")"));
-			
+
 			if (state == null) {
 				this.produce = null;
 			} else{
@@ -148,41 +149,41 @@ public class FarmModel extends PlotModel<FarmEnvironment>{
 			}
 		}
 	}
-	
-	
+
+
 	/**
 	 * Location that allows agents to enter on two different levels: ground and sky. While agents on different levels
 	 * can verbally interact because they are in the same geographical location, they can not interact physically.
 	 * Items can only be places below the tree, since they fall down if placed in the treetop.
-	 * 
+	 *
 	 * @author Leonid Berov
 	 */
 	public static class Tree extends Location {
 		public List<Character> treetop = null;
 		public List<Character> below = null;
-		
+
 		public Tree() {
 			super("tree");
-			this.treetop = new LinkedList<Character>();
-			this.below = new LinkedList<Character>();
+			this.treetop = new LinkedList<>();
+			this.below = new LinkedList<>();
 		}
-		
+
 		@Override
 		public boolean enter(Character character) {
 			boolean result;
-			
+
 			if (character.canFly()) {
 				result = this.enter(character, this.treetop);
 				logger.info(character.name + " flies to the top of an ancient tree");
-				
+
 				// characters in other places see the inventory of the character on the tree (apart from the character itself)
-				List<Character> otherPlacesChars = model.getCharacters().stream().filter(chara -> !chara.location.equals(this)).collect(Collectors.toList());
+				List<Character> otherPlacesChars = this.model.getCharacters().stream().filter(chara -> !chara.location.equals(this)).collect(Collectors.toList());
 				for (Character observer : otherPlacesChars) {
 					for (Item item : character.inventory) {
-						model.environment.addEventPercept(observer.getName(),
+						this.model.environment.addEventPercept(observer.getName(),
 															 "see(" + item.getItemName() + ")",
 															 new PerceptAnnotation().addAnnotation("location", this.name).addAnnotation("owner", character.name));
-						
+
 						logger.info(observer.name + " sees " + character.name + " with " + item);
 					}
 				}
@@ -190,10 +191,10 @@ public class FarmModel extends PlotModel<FarmEnvironment>{
 				result = this.enter(character, this.below);
 				logger.info(character.name + " stands below an ancient tree");
 			}
-			
+
 			return result;
 		}
-		
+
 		private boolean enter(Character character, List<Character> loc) {
 			if(null == character.location) {
 				// no prev location, just set this one
@@ -210,8 +211,8 @@ public class FarmModel extends PlotModel<FarmEnvironment>{
 				return false;
 			}
 		}
-		
-		
+
+
 		@Override
 		public boolean leave(Character character) {
 			if (this.present(character)) {
@@ -224,16 +225,16 @@ public class FarmModel extends PlotModel<FarmEnvironment>{
 				return false;
 			}
 		}
-		
+
 		@Override
 		public boolean present(Character character) {
-			return (this.treetop.contains(character) | this.below.contains(character));
+			return this.treetop.contains(character) | this.below.contains(character);
 		}
-		
+
 		@Override
 		public Literal createLocationPercept(Character character, Boolean otherChar) {
 			Literal loc = super.createLocationPercept(character, otherChar);
-			
+
 			if(this.treetop.contains(character)) {
 				loc.addAnnot(ASSyntax.createLiteral("level", ASSyntax.createAtom("sky")));
 			} else if (this.below.contains(character)){
@@ -242,10 +243,10 @@ public class FarmModel extends PlotModel<FarmEnvironment>{
 				logger.severe("Character "+ character.name +" not present at location " + this.name);
 				return null;
 			}
-			
+
 			return loc;
 		}
-		
+
 		@Override
 		public Boolean isGroundLevel(Character character) {
 			if (!this.present(character)) {
@@ -257,7 +258,7 @@ public class FarmModel extends PlotModel<FarmEnvironment>{
 				return false;
 			}
 		}
-		
+
 		@Override
 		public Boolean isSkyLevel(Character character) {
 			if (!this.present(character)) {
@@ -270,17 +271,17 @@ public class FarmModel extends PlotModel<FarmEnvironment>{
 			}
 		}
 	}
-	
+
 	public static class Wheat extends Item {
 		public static enum STATES {SEED, GROWING, RIPE, HARVESTED, FLOUR;}
 		public static final String itemName = "wheat";
-		
+
 		public STATES state = Wheat.STATES.SEED;
-		
+
 		@Override
 		public Literal literal() {
 			Literal res = super.literal();
-			res.addAnnot(ASSyntax.createLiteral("state", ASSyntax.createAtom(state.toString().toLowerCase())));
+			res.addAnnot(ASSyntax.createLiteral("state", ASSyntax.createAtom(this.state.toString().toLowerCase())));
 			return res;
 		}
 
@@ -290,15 +291,15 @@ public class FarmModel extends PlotModel<FarmEnvironment>{
 		}
 
 	}
-	
+
 	public static class Bread extends Item {
 		static final String itemName = "bread";
-		
+
 		@Override
 		public String getItemName() {
 			return Bread.itemName;
 		}
-		
+
 		@Override
 		public boolean isEdible() {
 			return true;
