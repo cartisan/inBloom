c468eaea1d11f49b4e8299ff172357f4c4fc658f
cartisan <leon@die-genossen.de>
2019-08-07 17:47:36 +0200

    Fixed bug in vertex ordering of plot graphs

--- src/java/inBloom/helper/VertexOrderComparator.java
+++ src/java/inBloom/helper/VertexOrderComparator.java
@@ -2,6 +2,9 @@ package inBloom.helper;
 
 import java.util.Comparator;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 import inBloom.graph.PlotDirectedSparseGraph;
 import inBloom.graph.Vertex;
@@ -13,6 +16,7 @@ import inBloom.graph.Vertex;
  * @author Leonid Berov
  */
 public class VertexOrderComparator implements Comparator<Vertex> {
+    static Logger logger = Logger.getLogger(VertexOrderComparator.class.getName());
 
 	private PlotDirectedSparseGraph graph;
 
@@ -26,19 +30,27 @@ public class VertexOrderComparator implements Comparator<Vertex> {
 			throw new RuntimeException("Vertices to be compared by order not part of the same graph");
 		}
 
+		logger.fine("Comparing: " + v1 + " and " + v2);
+
 		int stepOrder = Comparator.comparingInt(Vertex::getStep).compare(v1, v2);
 		// if vertices differ in step, their order is based on steps
 		if (stepOrder != 0) {
+			logger.fine("Result based on step order comparison: " + stepOrder);
 			return stepOrder;
 		}
+
 		// if step is the same, check if v2 is predecessor of v1 by following in-edges
 		LinkedList<Vertex> predsV1 = new LinkedList<>(this.graph.getRealPredecessors(v1));
 		while(!predsV1.isEmpty()) {
 			Vertex pred = predsV1.remove();
 			if(pred.equals(v2)) {
+				logger.fine("Result based on v1 predicessor search");
 				return 1;
 			}
-			predsV1.addAll(this.graph.getRealPredecessors(pred));
+			List<Vertex> sameStepPreds = this.graph.getRealPredecessors(pred).stream()
+																			 .filter(v -> v.getStep() == v1.getStep())
+																			 .collect(Collectors.toList());
+			predsV1.addAll(sameStepPreds);
 		}
 
 		// check if v1 is predecessor of v2 by following in-edges
@@ -46,12 +58,17 @@ public class VertexOrderComparator implements Comparator<Vertex> {
 		while(!predsV2.isEmpty()) {
 			Vertex pred = predsV2.remove();
 			if(pred.equals(v1)) {
+				logger.fine("Result based on v2 predicessor search");
 				return -1;
 			}
-			predsV2.addAll(this.graph.getRealPredecessors(pred));
+			List<Vertex> sameStepPreds = this.graph.getRealPredecessors(pred).stream()
+																			 .filter(v -> v.getStep() == v2.getStep())
+																			 .collect(Collectors.toList());
+			predsV2.addAll(sameStepPreds);
 		}
 
-		// if this fails too, order by label
-		return v1.getLabel().compareTo(v2.getLabel());
+		// if this fails too, order by id
+		logger.fine("Comparing by id");
+		return v1.getId().compareTo(v2.getId());
 	}
 }
