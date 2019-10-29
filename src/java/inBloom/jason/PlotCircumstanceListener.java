ab36adf96ea9ce984415d1dcd19465872bb2a963
cartisan <leon@die-genossen.de>
2019-10-29 16:52:04 +0100

    PlotCircumstanceListener filters emotion perceptions from being passed on as extra events.

--- src/java/inBloom/jason/PlotCircumstanceListener.java
+++ src/java/inBloom/jason/PlotCircumstanceListener.java
@@ -1,6 +1,12 @@
 package inBloom.jason;
 
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import jason.asSemantics.CircumstanceListener;
+import jason.asSemantics.Event;
+import jason.asSemantics.Intention;
 
 import inBloom.PlotLauncher;
 import inBloom.graph.Edge;
@@ -8,27 +14,32 @@ import inBloom.graph.PlotGraphController;
 import inBloom.graph.Vertex;
 import inBloom.graph.Vertex.Type;
 import inBloom.helper.TermParser;
-import jason.asSemantics.CircumstanceListener;
-import jason.asSemantics.Event;
-import jason.asSemantics.Intention;
 
 public class PlotCircumstanceListener implements CircumstanceListener {
 	static Logger logger = Logger.getLogger(PlotCircumstanceListener.class.getName());
-	
+
+	static Pattern EMO_PER_PATTERN = Pattern.compile("[+|-]emotion\\(\\w+\\)");
+
 	private PlotAwareAg agent;
 	private String name;
-	
+
 	public PlotCircumstanceListener(PlotAwareAg agent, String name) {
 		this.agent = agent;
 		this.name = name;
 	}
-	
+
 	@Override
 	public void eventAdded(Event e) {
+		// no need to keep +/-emotion(joy) events as percepts, we receive them as Type.EMOTION
+		Matcher m = EMO_PER_PATTERN.matcher(TermParser.removeAnnots(e.getTrigger().toString()));
+		if(m.matches()) {
+			return;
+		}
+
 		if(!e.getTrigger().isAchvGoal()) {
 			String toAdd = "";
 			String source = "";
-			// If it has source(self) (i.e. is mental note) and has an intention (i.e. a source within the agent's reasoning)
+			// If it has source(self) (i.e. is mental note) and has a parent event (stored by jason on the intention stack)
 			// then change the source from self to whatever the source is.
 			if(e.getTrigger().toString().endsWith("[source(self)]") && e.getIntention() != null) {
 				toAdd = e.getTrigger().toString().split("\\[source\\(self\\)\\]")[0];
@@ -38,7 +49,7 @@ public class PlotCircumstanceListener implements CircumstanceListener {
 			} else {
 				toAdd = e.getTrigger().toString();
 			}
-			PlotGraphController.getPlotListener().addEvent(name, toAdd, Vertex.Type.PERCEPT, PlotLauncher.getRunner().getUserEnvironment().getStep());
+			PlotGraphController.getPlotListener().addEvent(this.name, toAdd, Vertex.Type.PERCEPT, PlotLauncher.getRunner().getUserEnvironment().getStep());
 		}
 	}
 
@@ -55,12 +66,11 @@ public class PlotCircumstanceListener implements CircumstanceListener {
 			String cause = "" + this.agent.getTS().getC().getSelectedOption().getPlan().getTrigger();
 			PlotGraphController.getPlotListener().addEvent(this.name, "drop_intention(" + drop + ")[" + Edge.Type.CAUSALITY.toString() +"(" + TermParser.removeAnnots(cause) + ")]", Type.INTENTION, PlotLauncher.getRunner().getUserEnvironment().getStep());
 		}
-	}	
+	}
 
 	@Override
 	public void intentionSuspended(Intention i, String reason) { }
 
 	@Override
-	public void intentionResumed(Intention i) { }
-
+	public void intentionResumed(Intention i) {	}
 }
