a0ef4ef27e6ccf6f154d3c0de009d0a4038374ca
juwi <juwi@localhost>
2019-10-29 12:00:02 +0100

    Created content for the IslandLauncher Created first Robinson agent Error: Can't connect launcher to asl file

--- src/java/inBloom/rl_happening/IslandLauncher.java
+++ src/java/inBloom/rl_happening/IslandLauncher.java
@@ -3,7 +3,14 @@
  */
 package inBloom.rl_happening;
 
+import com.google.common.collect.ImmutableList;
+
+import inBloom.LauncherAgent;
+import inBloom.PlotControlsLauncher;
 import inBloom.PlotLauncher;
+import inBloom.storyworld.ScheduledHappeningDirector;
+import jason.asSemantics.Personality;
+import jason.infra.centralised.BaseCentralisedMAS;
 
 /**
  * @author Julia Wippermann
@@ -12,4 +19,32 @@ import inBloom.PlotLauncher;
  */
 public class IslandLauncher extends PlotLauncher<IslandEnvironment, IslandModel> {
 
+	public IslandLauncher() {
+		ENV_CLASS = IslandEnvironment.class;
+		PlotControlsLauncher.runner = this;
+		BaseCentralisedMAS.runner = this;
+	}
+	
+	public static void main(String[] args) {		
+		logger.info("Starting up from Launcher");
+		
+		PlotControlsLauncher.runner = new IslandLauncher();
+		
+		ImmutableList<LauncherAgent> agents = ImmutableList.of(
+				new LauncherAgent("robinson",
+						new Personality(0, 0, 0, 0, 0))
+				);
+		
+		// Initialise MAS with a scheduled happening director
+		ScheduledHappeningDirector hapDir = new ScheduledHappeningDirector();
+		
+		IslandModel model = new IslandModel(agents, hapDir);
+		
+		ribonson.location = model.island.name;
+		
+		// Execute MAS
+		// HERE IS THE LINK TO THE AGENT.ASL FILE!!!
+		runner.initialize(args, model, agents, islandAgent);
+		runner.run();
+	}
 }
