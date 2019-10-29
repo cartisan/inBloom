db77619f134ebc9c557cf62aba6dfddc1ab1d6e3
juwi <juwi@localhost>
2019-10-29 11:32:32 +0100

    Implement goOnCruise in Environment and Model

--- src/java/inBloom/rl_happening/IslandEnvironment.java
+++ src/java/inBloom/rl_happening/IslandEnvironment.java
@@ -5,13 +5,17 @@ package inBloom.rl_happening;
 
 import java.util.logging.Logger;
 
+import inBloom.ActionReport;
 import inBloom.PlotEnvironment;
 import inBloom.juwistest.HogwartsEnvironment;
+import inBloom.storyworld.Character;
+import jason.asSyntax.Structure;
 
 /**
  * @author Julia Wippermann
  * @version 29.10.19
  *
+ * The Environment defin
  */
 public class IslandEnvironment extends PlotEnvironment<IslandModel> {
 	
@@ -20,4 +24,15 @@ public class IslandEnvironment extends PlotEnvironment<IslandModel> {
 	// updateStatePercepts? -> gibt es in FarmEnvironment nicht mehr
 	// stattdessen initialize?
 
+	protected ActionReport doExecuteAction(String agentName, Structure action) {
+		
+		ActionReport result = null;
+		Character agent = getModel().getCharacter(agentName);
+		
+		if(action.getFunctor().equals("goOnCruise")) {
+			result = getModel().goOnCruise(agent);
+		}
+		
+		return result;
+	}
 }
--- src/java/inBloom/rl_happening/IslandModel.java
+++ src/java/inBloom/rl_happening/IslandModel.java
@@ -5,20 +5,49 @@ package inBloom.rl_happening;
 
 import java.util.List;
 
+import inBloom.ActionReport;
 import inBloom.LauncherAgent;
 import inBloom.PlotModel;
+import inBloom.helper.PerceptAnnotation;
 import inBloom.storyworld.HappeningDirector;
+import inBloom.storyworld.Character;
 
 /**
  * @author Julia Wippermann
  * @version 29.10.19
  *
+ * The Model defines the effects of Actions on the Environment and Agents.
  */
 public class IslandModel extends PlotModel<IslandEnvironment> {
-
+	
+	/**
+	 * GLOBAL VARIABLES
+	 */
+	
+	
+	/**
+	 * CONSTRUCTOR
+	 */
+	
 	public IslandModel(List<LauncherAgent> agentList, HappeningDirector hapDir) {
 		super(agentList, hapDir);
-		// TODO Auto-generated constructor stub
+		// here we could define our variables
+	}
+	
+	
+	
+	/**
+	 * ACTION METHODS
+	 */
+	
+	public ActionReport goOnCruise(Character agent) {
+		ActionReport result = new ActionReport();
+	
+		logger.info(agent.name + " went on a cruise.");
+		
+		result.addPerception(agent.name, new PerceptAnnotation("hope"));
+		result.success = true;
+		return result;
 	}
 
 }
