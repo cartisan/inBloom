bbe1a6ba52d3efe11333f84c7fc63b653edd7a99
juwi <juwi@localhost>
2020-08-04 14:16:02 +0200

    Attempt to automaise multiple runs
    not used!

---
+++ src/java/inBloom/rl_happening/rl_management/RLTrainer.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package inBloom.rl_happening.rl_management;
+
+/**
+ * Should run 100 or similar versions of the RL algorithm
+ * 
+ * @author juwi
+ *
+ */
+public class RLTrainer {
+
+	private static final int numberOfTrainings = 10;
+	private static final int numberOfEpisodes = 100;
+	
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		for(int i=1; i<=numberOfTrainings; i++) {
+			String fileName = generateFileName(i);
+			
+			// TODO hand over the #episodes
+			String[] robinsonArgs = {fileName};
+			
+			RobinsonCycleMultipleTrainings.main(robinsonArgs);
+		}
+
+	}
+	
+	
+	private static String generateFileName(int training) {
+		String fileName = "episodes" + training + ".csv";
+		return fileName;
+	}
+
+}
